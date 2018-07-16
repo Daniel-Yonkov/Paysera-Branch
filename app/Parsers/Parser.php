fe1e22a8bfe8a5ad350d773cce338b9e92d3550d
Daniel-Yonkov <daniel.yonkov@gmail.com>
2018-07-16 12:23:11 +0300

    added explanation to Parser interface

--- app/Parsers/Parser.php
+++ app/Parsers/Parser.php
@@ -1,6 +1,11 @@
 <?php
 namespace Paysera\Branch\Parsers;
-
+/**
+ * Implementing this interface allows to parse data from different sources 
+ * (CSV, XLS, Relation/Document based database, xml, etc).
+ * Just create your parsing logic within getData method,
+ * based on the received data
+ */
 interface Parser
 {
 	public function getData($data);

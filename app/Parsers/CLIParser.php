ac3f45fd26861fec9500e9025675667ce7d784f1
Daniel-Yonkov <daniel.yonkov@gmail.com>
2018-07-17 11:28:34 +0300

    moved parsers logic to the abstract class for default functionality support

--- app/Parsers/CLIParser.php
+++ app/Parsers/CLIParser.php
@@ -1,29 +1,4 @@
 <?php
 namespace Paysera\Branch\Parsers;
 
-class CLIParser implements Parser 
-{
-	protected $keys = array('date','user_id','user_type','operation_type','amount','currency');
-	/**
-	 * Array based parsing for CSV files trought CLI
-	 * @param string $filename
-	 * @return array - multidimensional
-	 */
-	public function getData($filename)
-	{
-		$file = $filename;
-		if(is_readable($filename)) $file = file_get_contents($filename);
-		$file = explode(PHP_EOL,$file);
-		return array_map([STATIC::CLASS,'parse'],$file);
-	}
-	/**
-	 * Parses the provided csv file into multidimensional array with parent array = row, child array = value
-	 * @param array $value - readed file 
-	 * @return array multidimensional
-	 */
-	protected function parse($value)
-	{
-		$val = explode(',',$value);
-		return array_combine($this->keys,array_values($val));
-	}
-}
+class CLIParser Extends Parser {};

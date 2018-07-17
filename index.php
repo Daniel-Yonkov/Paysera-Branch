abac9a7a41322406f42eca02c7526a8f7ae70997
Daniel-Yonkov <daniel.yonkov@gmail.com>
2018-07-17 11:29:23 +0300

    accessing default library handler - (TransactionController)

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
 <?php
 require_once 'vendor/autoload.php';
 
-use Paysera\Branch\Input;
+use Paysera\Branch\Controllers\TransactionController;
+use Paysera\Branch\Converters\EuroConverter;
 use Paysera\Branch\Parsers\CLIParser;
-
-$input = Input::data($argv[1],new CLIParser());
-var_dump($input);
+//main report builder class
+$controller = new TransactionController();
+//we setup the converter for the currency we want to check the limit per week
+$controller->setConverter(new EuroConverter());
+//we run the results for the taxes by providing either a csv file or a string and the parser of the file
+$result = $controller->run($argv[1],new CLIParser());

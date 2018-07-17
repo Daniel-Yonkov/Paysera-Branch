487e26f40a9d9b8835269d0101eeb541d50ab21b
Daniel-Yonkov <daniel.yonkov@gmail.com>
2018-07-17 18:02:22 +0300

    changed setup of default currency and currency rate

--- app/Converters/Converter.php
+++ app/Converters/Converter.php
@@ -3,11 +3,32 @@ namespace Paysera\Branch\Converters;
 
 abstract class Converter
 {
+	public function __construct()
+	{
+		//EUR:USD - 1:1.1497, EUR:JPY - 1:129.53
+		//Currencly it supports only one rate for the default currency, 
+		
+		$this->setRates($this->defineCurrencyRates());
+		$this->defaultCurrency = $this->defineDefaultCurrency();
+	}
+	protected $defaultCurrency;
 	protected $currenciesRate = array();
-	public function convert($value, string $convertFrom, string $convertTo = "EUR"){
-		if($convertFrom === 'EUR') $result = $value;
-		elseif($convertTo === 'EUR' && isset($this->currenciesRate[1][$convertFrom])){
-			$result = $value/$this->currenciesRate[1][$convertFrom];
+	public function convert($value, string $convertFrom, string $convertTo = null){
+		//check if conversion is needed (currency is the same as default currency)
+		if($convertFrom === $this->defaultCurrency && ($convertTo === $this->defaultCurrency || $convertTo === null)){
+			$result = $value;
+		}
+		//converts supported currency to default currency if it exists
+		elseif(($convertTo === null || $convertTo === $this->defaultCurrency) && isset($this->currenciesRate[key($this->currenciesRate)][$convertFrom])){
+			$result = 
+			($value*key($this->currenciesRate))
+			/
+			$this->currenciesRate[key($this->currenciesRate)][$convertFrom];
+		}
+		//conversion from defaut currency to supported one
+		elseif(($convertFrom === $this->defaultCurrency && $convertTo !== $this->defaultCurrency) && isset($this->currenciesRate[key($this->currenciesRate)][$convertTo]))
+		{
+			$result = $value*key($this->currenciesRate)*$this->currenciesRate[key($this->currenciesRate)][$convertTo];
 		}
 		/** 
 		 * Tries to convert Convert From Currency to Convert To Currency
@@ -45,8 +66,6 @@ abstract class Converter
 	{
 		$this->currenciesRate = $rates;
 	}
-	public function getRates() : array
-	{
-		return $this->rates;
-	}
+	public abstract function defineCurrencyRates();
+	public abstract function defineDefaultCurrency();
 }
--- app/Converters/EuroConverter.php
+++ app/Converters/EuroConverter.php
@@ -3,15 +3,17 @@ namespace Paysera\Branch\Converters;
 
 class EuroConverter extends Converter
 {
-	public function __construct()
+	public function defineDefaultCurrency()
 	{
-		//EUR:USD - 1:1.1497, EUR:JPY - 1:129.53
-		$rates = array(
-			1 => array(
-				'JPY' => 129.53,
-				'USD' => 1.1497
-			),
-		);
-		$this->setRates($rates);
+		return 'EUR';
+	}
+	public function defineCurrencyRates()
+	{
+		return array(
+				1 => array(
+					'JPY' => 129.53,
+					'USD' => 1.1497
+					),
+				);
 	}
 }

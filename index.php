053d4db6e988171f64df1090e40c32b061277632
Daniel-Yonkov <daniel.yonkov@gmail.com>
2018-07-17 18:06:47 +0300

    Added call to the library implementation

--- index.php
+++ index.php
@@ -3,10 +3,12 @@ require_once 'vendor/autoload.php';
 
 use Paysera\Branch\Controllers\TransactionController;
 use Paysera\Branch\Converters\EuroConverter;
+use Paysera\Branch\Output\CLIReport;
 use Paysera\Branch\Parsers\CLIParser;
 //main report builder class
 $controller = new TransactionController();
 //we setup the converter for the currency we want to check the limit per week
 $controller->setConverter(new EuroConverter());
 //we run the results for the taxes by providing either a csv file or a string and the parser of the file
+$controller->setOutput(new CLIReport(STDOUT));
 $result = $controller->run($argv[1],new CLIParser());

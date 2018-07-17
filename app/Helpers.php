869ab408c9b6e7a482c2a38e7e727e234deda451
Daniel-Yonkov <daniel.yonkov@gmail.com>
2018-07-17 18:02:37 +0300

    added helper function

--- app/Helpers.php
+++ app/Helpers.php
@@ -3,3 +3,14 @@
 function yearlyWeek($date){
 	return date('o_W',strtotime($date));
 }
+/**
+ * Taken from PHP.net - https://secure.php.net/manual/en/function.ceil.php#50448
+ * @param float $value 
+ * @param int $places 
+ * @return string
+ */
+function roundUp ($value, $places=2) {
+  if ($places < 0) { $places = 0; }
+  $mult = pow(10, $places);
+  return number_format(ceil($value * $mult) / $mult, 2, '.', '');
+}
